6651037fd0ec0ec7474927fe1d8ec1000b9db9e5
Vladko Fateich <Fateev_wbd@mail.ru>
2024-09-02 21:25:09 +0500

    add timer

--- script.js
+++ script.js
@@ -10,8 +10,17 @@ const btnGenerate = document.createElement('button');
 const btnNewGame = document.createElement('button');
 const title = document.createElement('h1');
 const subTitle = document.createElement('p');
+const timer = document.createElement('div');
+const animation = document.querySelector('#animation');
+const minutes = document.createElement('span');
+const seconds = document.createElement('span');
 
-let arr = [];
+timer.id = 'timerId';
+minutes.id = 'minutes';
+seconds.id = 'seconds';
+
+timer.append(minutes);
+timer.append(seconds);
 
 for (let i = 0; i <= txtArr.length; i++) {
   btnGenerate.textContent = txtArr[0];
@@ -22,21 +31,86 @@ for (let i = 0; i <= txtArr.length; i++) {
   wrapper.append(subTitle);
   wrapper.append(btnGenerate);
   wrapper.append(btnNewGame);
+  wrapper.append(animation);
+  wrapper.append(timer);
 }
-createTable(10, 10, wrapper);
-const tds = document.querySelectorAll('td');
 
-btnNewGame.addEventListener('click', restartGame);
-function restartGame() {
-  for (let elem of tds) {
-    elem.style.background = 'white';
+let arr = [];
+
+// на сколько минут ставим таймер
+let count = 1;
+// запущен таймер или нет
+let started = false;
+
+// запуск таймера по кнопке
+function startTimer() {
+  let table = document.querySelector('table');
+  // если таймер уже запущен — выходим из функции
+  if (started) {
+    return;
   }
+  // запоминаем время нажатия
+  let start = new Date();
+  console.log(start);
+  // получаем время окончания таймера
+  let stop = start.setMinutes(start.getMinutes() + count);
+  console.log(stop);
+
+  // запускаем ежесекундный отсчёт
+  let countDownTimer = setInterval(function () {
+    // текущее время
+    let now = new Date().getTime();
+    // сколько времени осталось до конца таймера
+    let remain = stop - now;
+    // переводим миллисекунды в минуты и секунды
+    const m = Math.floor((remain / 1000 / 60) % 60);
+    const s = Math.floor((remain / 1000) % 60);
+    // если значение текущей секунды меньше 10, добавляем вначале ведущий ноль
+    minutes.textContent = m < 10 ? '0' + m : m;
+    // console.log(minutes)
+    seconds.textContent = s < 10 ? '0' + s : s;
+    // console.log(minutes)
+    // если время вышло
+    if (remain < 0) {
+      // останавливаем отсчёт
+      clearInterval(countDownTimer);
+      // пишем текст вместо цифр
+      timer.textContent = 'STOP GAME';
+      table.remove();
+      btnNewGame.addEventListener('click', restartGame);
+      // btnGenerate.addEventListener('click', start);
+    }
+  }, 1000);
+  // помечаем, что таймер уже запущен
+  started = true;
+}
+
+// createTable(10, 10, wrapper);
+// const tds = document.querySelectorAll('td');
+
+// btnNewGame.addEventListener('click', restartGame);
+
+// function restartGame() {
+//   animation.style.display = 'inline-block';
+//   for (let elem of tds) {
+//     elem.style.background = 'white';
+//   }
+//   arr = [];
+//   btnGenerate.addEventListener('click', start);
+// }
+function restartGame() {
+  // timer.textContent = '';
   arr = [];
   btnGenerate.addEventListener('click', start);
 }
+
 btnGenerate.addEventListener('click', start);
 
 function start() {
+  animation.style.display = 'none';
+  createTable(10, 10, wrapper);
+  startTimer();
+  const tds = document.querySelectorAll('td');
   for (let i = 0; i < tds.length; i++) {
     tds[i].addEventListener('click', function () {
       if (arr.includes(i)) {
@@ -47,10 +121,9 @@ function start() {
     });
   }
   console.log(arr);
-
   for (let i = 0; i < 10; i++) {
     arr.push(getArrayRandom(1, 100));
-    this.removeEventListener('click', start);
+    btnGenerate.removeEventListener('click', start);
   }
 }
 
